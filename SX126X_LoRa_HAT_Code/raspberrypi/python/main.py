fc295fa268f9bd660f0b4ed6de6c5026246b2e53
Lemuel-Noah-Fernandez <87133989+Lemuel-Noah-Fernandez@users.noreply.github.com>
2024-04-24 12:38:13 +1000

    Send deal revamp

--- SX126X_LoRa_HAT_Code/raspberrypi/python/main.py
+++ SX126X_LoRa_HAT_Code/raspberrypi/python/main.py
@@ -67,40 +67,64 @@ def get_cpu_temp():
 node = sx126x.sx126x(serial_num = "/dev/ttyS0",freq=433,addr=0,power=22,rssi=False,air_speed=2400,relay=False)
 #node = sx126x.sx126x(serial_num = "/dev/ttyS0",freq=868,addr=0,power=22,rssi=True,air_speed=2400,relay=False)
 
-def send_deal():
-    get_rec = ""
-    print("")
-    # print("input a string such as \033[1;32m0,868,Hello World\033[0m,it will send `Hello World` to lora node device of address 0 with 868M ")
-    print("Please input and press Enter key:",end='',flush=True)
-    DEFAULT_ADDRESS = 0
-    FREQ_433 = 433
+# def send_deal():
+#     get_rec = ""
+#     print("")
+#     # print("input a string such as \033[1;32m0,868,Hello World\033[0m,it will send `Hello World` to lora node device of address 0 with 868M ")
+#     print("Please input and press Enter key: ",end='',flush=True)
+#     DEFAULT_ADDRESS = 0
+#     FREQ_433 = 433
     
 
-    while True:
-        rec = sys.stdin.read(1)
-        if rec != None:
-            if rec == '\x0a': break
-            get_rec += rec
-            sys.stdout.write(rec)
-            sys.stdout.flush()
+#     while True:
+#         rec = sys.stdin.read(1)
+#         if rec != None:
+#             if rec == '\x0a': break
+#             get_rec += rec
+#             sys.stdout.write(rec)
+#             sys.stdout.flush()
 
-    get_t = get_rec.split(",")
+#     get_t = get_rec.split(",")
 
-    offset_frequence = int(FREQ_433)-(850 if int(FREQ_433)>850 else 410)
-    #
-    # the sending message format
-    #
-    #         receiving node              receiving node                   receiving node           own high 8bit           own low 8bit                 own 
-    #         high 8bit address           low 8bit address                    frequency                address                 address                  frequency             message payload
-    # data = bytes([int(get_t[0])>>8]) + bytes([int(get_t[0])&0xff]) + bytes([offset_frequence]) + bytes([node.addr>>8]) + bytes([node.addr&0xff]) + bytes([node.offset_freq]) + get_t[2].encode()
-    data = bytes([int(DEFAULT_ADDRESS)>>8]) + bytes([int(DEFAULT_ADDRESS)&0xff]) + bytes([offset_frequence]) + bytes([node.addr>>8]) + bytes([node.addr&0xff]) + bytes([node.offset_freq]) + get_t[2].encode()
+#     offset_frequence = int(FREQ_433)-(850 if int(FREQ_433)>850 else 410)
+#     #
+#     # the sending message format
+#     #
+#     #         receiving node              receiving node                   receiving node           own high 8bit           own low 8bit                 own 
+#     #         high 8bit address           low 8bit address                    frequency                address                 address                  frequency             message payload
+#     # data = bytes([int(get_t[0])>>8]) + bytes([int(get_t[0])&0xff]) + bytes([offset_frequence]) + bytes([node.addr>>8]) + bytes([node.addr&0xff]) + bytes([node.offset_freq]) + get_t[2].encode()
+#     data = bytes([int(DEFAULT_ADDRESS)>>8]) + bytes([int(DEFAULT_ADDRESS)&0xff]) + bytes([offset_frequence]) + bytes([node.addr>>8]) + bytes([node.addr&0xff]) + bytes([node.offset_freq]) + get_t[2].encode()
+
+#     node.send(data)
+#     print('\x1b[2A',end='\r')
+#     print(" "*200)
+#     print(" "*200)
+#     print(" "*200)
+#     print('\x1b[3A',end='\r')
+
+def send_deal():
+    print("\nPlease input your message and press Enter key: ", end='')
+    message = input()  # Get the entire line of input at once.
+
+    # Constants for address and frequency
+    DEFAULT_ADDRESS = 0
+    FREQ_433 = 433
+
+    # Calculate the offset frequency
+    offset_frequency = FREQ_433 - (850 if FREQ_433 > 850 else 410)
+
+    # Prepare the data packet
+    data = (bytes([DEFAULT_ADDRESS >> 8]) + 
+            bytes([DEFAULT_ADDRESS & 0xff]) +
+            bytes([offset_frequency]) + 
+            bytes([node.addr >> 8]) + 
+            bytes([node.addr & 0xff]) + 
+            bytes([node.offset_freq]) + 
+            message.encode())
 
     node.send(data)
-    print('\x1b[2A',end='\r')
-    print(" "*200)
-    print(" "*200)
-    print(" "*200)
-    print('\x1b[3A',end='\r')
+    print("Message sent!")
+
 
 def send_cpu_continue(continue_or_not = True):
     if continue_or_not:
